d223a005a28767af4d801ef47aa59a215697c6a1
ryunosuke_ide <ryumyu0120@gmail.com>
2018-03-05 02:31:57 +0900

    feat:パラパラ漫画と音楽がなるようにする

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(function(){
+	document.getElementById("overSound").currentTime = 0; //再生秒数を 0 にセット
+	document.getElementById("overSound").volume = 0.1;
+	document.getElementById("overSound").play();
+	
+	var imgIdx = 0;
+	setInterval(function(){
+		imgIdx = (imgIdx + 1) % 6;
+		$('img').attr('src', '../resources/img/0' + (imgIdx + 1) + '.png');
+	},1000);
+});
