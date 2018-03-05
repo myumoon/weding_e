7daf6d6f2168c0cd9a4477635dd90f52bacc2a52
ryunosuke_ide <ryumyu0120@gmail.com>
2018-03-05 23:08:44 +0900

    feat:スマホではロード時に自動で実行することはできないらしいのでボタンを付ける

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,4 +1,11 @@
 $(function(){
+	// スマホではユーザー入力じゃないと動かないらしいのでここは使わない
+});
+
+$("#button_start").click(function(e) {
+	$("#main_image").css("display", "inline");
+	$(this).css("display", "none");
+	
 	document.getElementById("overSound").currentTime = 0; //再生秒数を 0 にセット
 	document.getElementById("overSound").volume = 0.1;
 	document.getElementById("overSound").play();
