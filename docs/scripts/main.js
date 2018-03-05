0f49af4a66fbe7aeaf367e34a84faaecce8d8238
ryunosuke_ide <ryumyu0120@gmail.com>
2018-03-05 23:42:48 +0900

    feat:アクティブじゃないときは音を止めるようにする

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -2,13 +2,18 @@ $(function(){
 	// スマホではユーザー入力じゃないと動かないらしいのでここは使わない
 });
 
+var bgm = document.getElementById("overSound");
+var bttonClicked = false;
+
 $("#button_start").click(function(e) {
 	$("#main_image").css("display", "inline");
 	$(this).css("display", "none");
 	
-	document.getElementById("overSound").currentTime = 0; //再生秒数を 0 にセット
-	document.getElementById("overSound").volume = 0.1;
-	document.getElementById("overSound").play();
+	bgm.currentTime = 0; //再生秒数を 0 にセット
+	bgm.volume = 0.1;
+	bgm.play();
+	
+	bttonClicked = true;
 	
 	var imgIdx = 0;
 	setInterval(function(){
@@ -16,3 +21,15 @@ $("#button_start").click(function(e) {
 		$('img').attr('src', 'resources/img/0' + (imgIdx + 1) + '.jpg');
 	},1000);
 });
+
+document.addEventListener('visibilitychange', function() {
+	if(document.visibilityState === 'hidden') {
+		if(bttonClicked === true) {
+			bgm.pause();
+		}
+	}else if(document.visibilityState === 'visible') {
+		if(bttonClicked === true) {
+			bgm.play();
+		}
+	}
+}, false);
